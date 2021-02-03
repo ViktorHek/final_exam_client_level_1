fe488ed75a052dc81cfabe98f896f08837e9961b
viktor <viktorkarlsson125@gmail.com>
2021-02-03 16:50:37 +0100

    changing size on image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div style={{color: "black"}}>
+    <div style={{color: "black", grid: "flex"}}>
       <h1>VIAPLAY</h1>
-      <img src={"https://kundservice.viaplay.se/wp-content/themes/viaplaycs/assets/dist/images/viaplay_white.svg"}/>
+      <img src={"https://kundservice.viaplay.se/wp-content/themes/viaplaycs/assets/dist/images/viaplay_white.svg"} widht="40" height="40"/>
     </div>
   )
 }
